787b86de7bca28d7bd9bfa23b962281b315e5ab2
Ahad-Rehman <ahadr4592@gmail.com>
2026-01-18 23:14:20 +0500

    commited by ahad

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Portfolio",
+  title: "Bambar Group Portfolio",
   description: "My personal portfolio",
 };
 
